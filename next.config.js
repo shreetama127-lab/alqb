249e65b3e625c012f862193bd66a1bdd5b45bfd8
Shreetama <shreetama127@gmail.com>
2026-09-09 22:23:26 +0100

    config and question updates

---
+++ next.config.js
@@ -0,0 +1,13 @@
+﻿const path = require("path");
+const nextConfig = {
+  turbopack: {
+    resolveAlias: {
+      "@": path.resolve(__dirname),
+    },
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
+};
+module.exports = nextConfig;
